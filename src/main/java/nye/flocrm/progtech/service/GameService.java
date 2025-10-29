58b7111cc8ad052f9e47316007c1a7616ec53a14
balintbotond89 <balint.botond@student.nye.hu>
2025-10-29 19:37:13 +0100

    20. BB: Started Main application class and changes in GameService.

--- src/main/java/nye/flocrm/progtech/service/GameService.java
+++ src/main/java/nye/flocrm/progtech/service/GameService.java
@@ -1,4 +1,40 @@
 package nye.flocrm.progtech.service;
 
+import nye.flocrm.progtech.model.*;
+
+/**
+ * A fő játékmódokat megvalósító játékszolgáltatás.
+ */
 public class GameService {
+    private final Board board;
+    private final WinChecker winChecker;
+    private GameState gameState;
+    private Player currentPlayer;
+    private Player player1;
+    private Player player2;
+    private GameMode gameMode;
+
+    public GameService(GameMode gameMode) {
+        this.board = new Board();
+        this.winChecker = new WinChecker();
+        this.gameState = GameState.IN_PROGRESS;
+        this.gameMode = gameMode;
+        initializePlayers();
+    }
+
+    private void initializePlayers() {
+        switch (gameMode) {
+            case HUMAN_VS_HUMAN:
+                this.player1 = new HumanPlayer("Játkos 1", 'X');
+                this.player2 = new HumanPlayer("Játékos 2", 'O');
+                break;
+            case HUMAN_VS_AI:
+                    this.player1 = new HumanPlayer("Ember", 'X');
+                this.player2 = new AIPlayer("Számítógép", 'O', 'X');
+                break;
+        }
+        this.currentPlayer = player1;
+    }
+
+
 }
--- src/main/java/nye/flocrm/progtech/ui/Main.java
+++ src/main/java/nye/flocrm/progtech/ui/Main.java
@@ -1,17 +1,17 @@
 //Beadási határidő: december 5
 
 package nye.flocrm.progtech.ui;
-import nye.flocrm.progtech.model.Board;
+
+import nye.flocrm.progtech.service.GameService;
+import nye.flocrm.progtech.model.GameMode;
+import nye.flocrm.progtech.model.GameState;
+
+import java.util.Scanner;
+
+/**
+ * Az amőba játék fő alkalmazás osztálya.
+ */
 
 @SuppressWarnings({"PMD.SystemPrintln"})
 public class Main {
-
-    // utility osztály (ne lehessen példányosítani)
-    private Main() { }
-
-    public static void main(String[] args) {
-        Board board = new Board();
-        board.print();
-
-    }
 }
