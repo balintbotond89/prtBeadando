1fe6cc9871462829fc0bd2a01252f874e78aa030
balintbotond89 <balint.botond@student.nye.hu>
2025-10-30 08:32:09 +0100

    23. BB: Started implementing Main menu and core game funtctions.

--- src/main/java/nye/flocrm/progtech/ui/Main.java
+++ src/main/java/nye/flocrm/progtech/ui/Main.java
@@ -14,4 +14,59 @@ import java.util.Scanner;
 
 @SuppressWarnings({"PMD.SystemPrintln"})
 public class Main {
+
+    private GameService gameService;
+    private final Scanner scanner;
+
+    public Main() {
+        this.scanner = new Scanner(System.in);
+    }
+
+    public static void main(String[] args) {
+        new Main().run();
+    }
+
+    //run
+    public void run() {
+        System.out.println("Isten hozott az amőba játékban!");
+        System.out.println("Rakj le 5 jelet egy sorban a győzelemhez!\n");
+
+        //selectGameMode();
+        //gameLoop();
+
+        scanner.close();
+    }
+
+    //selectGameMode
+
+    //gameLoop
+
+    //displayGameResult
+
+    //offerNewGame
+
+    //Select game mode
+
+    /**
+     * Felhasználói menü választást olvas be és érvényesít a konzolról.
+     * A metódus egy egyszerű menürendszert valosít meg, ahol
+     * a felhasználónak egy előre meghatározott tartományból kell választania.
+     * A metódus addig kér be újabb inputot, amíg a felhasználó érvényes számot nem ad meg
+     * a megadott határok között.
+     */
+    private int getMenuChoice(int min, int max) {
+        while (true) {
+            try {
+                int choice = scanner.nextInt();
+                scanner.nextLine(); // puffer űrítése
+                if (choice >= min && choice <= max) {
+                    return choice;
+                }
+                System.out.print("Kérem adjon meg egy számot a  " + min + " és " + max + "között: ");
+            } catch (Exception e) {
+                System.out.print("Érvénytelen bevitel! Kérem adjon meg egy érvényes számot: ");
+                scanner.nextLine(); // Helytelen bemenet elvetése
+            }
+        }
+    }
 }
